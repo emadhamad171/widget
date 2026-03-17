85e2473177614e210c447f02fa0ae471f7c43228
emadhamad171 <dusikeh@gmail.com>
2026-03-17 13:08:21 +0200

    Updates

--- services/api.d.ts
+++
@@ -1,3 +0,0 @@
-import { HotPotApiClient } from '@hot-pot/hotpot-sdk-ts';
-export declare const hotpotApiClient: HotPotApiClient;
-//# sourceMappingURL=api.d.ts.map
--- widget.es.js
+++
@@ -1,5 +0,0 @@
-import { h as p } from "./index-Crwdt6pm.mjs";
-export {
-  p as HotpotSwapWidget
-};
-//# sourceMappingURL=widget.es.js.map
